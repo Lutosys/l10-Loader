966b5adacf337388895570a39ee576bed9630c29
Lutosys <nickgaming271@gmail.com>
2025-12-24 16:07:17 -0500

    added loader

---
+++ hub.lua
@@ -0,0 +1,40 @@
+ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"))()
+
+repeat
+    task.wait()
+until game.Loaded
+
+local MarketplaceService = game:GetService("MarketplaceService")
+
+local success, info = pcall(function()
+    return MarketplaceService:GetProductInfo(game.PlaceId, Enum.InfoType.Asset)
+end)
+
+local supportedgames = {
+    ["Prison Life"] = {
+        getscript = "https://api.junkie-development.de/api/v1/luascripts/public/2c266d71a55eb41d24d3bb343b78611771cf73f80d0d657e1b1923cd16322e0b/download"
+    }
+}
+
+local function isSupportedGame(gameName)
+    for pattern, data in pairs(supportedgames) do
+        if string.find(gameName, pattern) then
+            Library:Notify("Game: "..gameName.." is supported running script!", 5)
+            return loadstring(game:HttpGet(data.getscript))()
+        end
+    end
+    Library:Notify("Game: "..gameName.." is not supported!", 5)
+
+end
+
+local function runScript()
+    if success and info then
+        local gameName = info.Name
+        Library:Notify("Checking if: "..gameName.." is supported!", 5)
+        isSupportedGame(gameName)
+    else
+        Library:Notify("Could not get place name!", 5)
+    end
+end
+
+runScript()
