d2b28c8fb878211ee67cd002068848be814db2a8
Lutosys <nickgaming271@gmail.com>
2026-01-12 17:42:45 -0500

    Update script URLs for supported games

--- hub.lua
+++ hub.lua
@@ -13,16 +13,16 @@ end)
 
 local supportedgames = {
     ["Prison Life"] = {
-        getscript = "https://api.junkie-development.de/api/v1/luascripts/public/22d4214e484466ad8bdfa13b144f06071721e38d284dee95aef2d74488f7d07a/download"
+        getscript = "https://api.jnkie.com/api/v1/luascripts/public/22d4214e484466ad8bdfa13b144f06071721e38d284dee95aef2d74488f7d07a/download"
     },
     ["Blind Shot"] = {
-       getscript = "https://api.junkie-development.de/api/v1/luascripts/public/24e6f43a500392a7bfd6922b14da0d35b51a677954d33a6aaa37907f08bed9d4/download"
+       getscript = "https://api.jnkie.com/api/v1/luascripts/public/24e6f43a500392a7bfd6922b14da0d35b51a677954d33a6aaa37907f08bed9d4/download"
    },
     ["Cart Ride"] = {
-       getscript = "https://api.junkie-development.de/api/v1/luascripts/public/6b99ff80d5ff2d44e542cbdeaf8e7e187363555fc318fe645a68884855cb8303/download"
+       getscript = "https://api.jnkie.com/api/v1/luascripts/public/6b99ff80d5ff2d44e542cbdeaf8e7e187363555fc318fe645a68884855cb8303/download"
    },
    ["Da Hood"] = {
-       getscript = "https://api.junkie-development.de/api/v1/luascripts/public/c85c36dacb8bce4e1704e86d38734a80409eab3b6e4077ffe675752717493f30/download"
+       getscript = "https://api.jnkie.com/api/v1/luascripts/public/c85c36dacb8bce4e1704e86d38734a80409eab3b6e4077ffe675752717493f30/download"
    },
    ["Flick"] = {
        getscript = "https://pastebin.com/raw/SBbNE9sG"
